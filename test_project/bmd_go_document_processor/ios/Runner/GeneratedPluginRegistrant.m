77ac0dcd351549446e50620bf59551ad0d3a583e
L-Schlosser <lorenz.schlosser@gmx.net>
2026-03-19 10:42:10 +0100

    update project

--- test_project/bmd_go_document_processor/ios/Runner/GeneratedPluginRegistrant.m
+++ test_project/bmd_go_document_processor/ios/Runner/GeneratedPluginRegistrant.m
@@ -18,27 +18,6 @@
 @import file_picker;
 #endif
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
->>>>>>> temp_mobileapp
-#if __has_include(<google_mlkit_commons/GoogleMlKitCommonsPlugin.h>)
-#import <google_mlkit_commons/GoogleMlKitCommonsPlugin.h>
-#else
-@import google_mlkit_commons;
-#endif
-
-#if __has_include(<google_mlkit_text_recognition/GoogleMlKitTextRecognitionPlugin.h>)
-#import <google_mlkit_text_recognition/GoogleMlKitTextRecognitionPlugin.h>
-#else
-@import google_mlkit_text_recognition;
-#endif
-
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> temp_mobileapp
 #if __has_include(<image_picker_ios/FLTImagePickerPlugin.h>)
 #import <image_picker_ios/FLTImagePickerPlugin.h>
 #else
@@ -56,16 +35,6 @@
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [CameraPlugin registerWithRegistrar:[registry registrarForPlugin:@"CameraPlugin"]];
   [FilePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FilePickerPlugin"]];
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-  [GoogleMlKitCommonsPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitCommonsPlugin"]];
-  [GoogleMlKitTextRecognitionPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitTextRecognitionPlugin"]];
-=======
->>>>>>> Stashed changes
-=======
-  [GoogleMlKitCommonsPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitCommonsPlugin"]];
-  [GoogleMlKitTextRecognitionPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitTextRecognitionPlugin"]];
->>>>>>> temp_mobileapp
   [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
   [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
 }
